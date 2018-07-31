1c0326d7e8160cc4e086fedbd789ac29bfe2a77b
József Tar <szurilo@gmail.com>
2018-07-31 23:09:01 +0200

    fine

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-$(function(){
+$(function() {
+  var canvas = document.getElementById("MyCanvas");
+  var context = canvas.getContext("2d");
 
-    var canvas = document.getElementById('MyCanvas');
-    var context = canvas.getContext('2d');
-
-    var side = 0;
+  function hexagon(x, y) {
     var size = 40;
-    var x = 100;
-    var y = 100;
 
-   
-context.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
+    context.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
 
-for (side; side < 7; side++) {
-  context.lineTo(x + size * Math.cos(side * 2 * Math.PI / 6), y + size * Math.sin(side * 2 * Math.PI / 6));
-}
+    for (side = 0; side < 7; side++) {
+      context.lineTo(
+        x + size * Math.cos((side * 2 * Math.PI) / 6),
+        y + size * Math.sin((side * 2 * Math.PI) / 6)
+      );
+      context.strokeStyle = "#ffa100";
+      context.stroke();
+    }
+  }
 
-context.strokeStyle = "#ffa100";
-context.stroke();
-
+  hexagon(240, 180);
+  context.fillStyle = "#ffa100";
+  context.fill();
+  hexagon(100, 180);
+  hexagon(170, 60);
+  hexagon(170, 140);
+  hexagon(170, 220);
+  hexagon(240, 100);
+  hexagon(100, 100);
 });
